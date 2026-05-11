837a8ecf906c83d564428e2b8b381c94baf4f6c0
Your Name <you@example.com>
2026-05-11 11:11:03 -0700

    chore: update build helper

--- build.sh
+++ build.sh
@@ -31,7 +31,13 @@ fi
 
 flutter pub get && flutter pub upgrade >> $build_log_file 2>&1
 if [[ $* == *"--ffigen"* ]]; then
-    cd $script_dir/flutter && dart run ffigen --config ffigen.yaml && cd example >> $build_log_file 2>&1 
+    cd $script_dir/flutter
+    if [[ ! -f ".dart_tool/ffmpeg_kit_extended_flutter/include/ffmpegkit_wrapper.h" ]]; then
+        mkdir -p .dart_tool/ffmpeg_kit_extended_flutter/include
+        header_dir=$(find './example/.dart_tool/hooks_runner/shared/ffmpeg_kit_extended_flutter/build/ffmpeg_kit_cache' -name 'include' -type d | head -n 1)
+        cp -r "$header_dir" .dart_tool/ffmpeg_kit_extended_flutter/
+    fi
+    cd $script_dir/flutter && dart run ffigen --config ffigen.yaml && cd example >> $build_log_file 2>&1
 fi
 if [[ $* == *"-y"* ]]; then
     flutter build -v $platform $2 >> $build_log_file 2>&1 || cd $script_dir
